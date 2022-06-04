2bf792f26313e44fc03e94d847bd39f1f3bce0e5
palak patel <palakp909@gmail.com>
2022-06-04 14:41:23 -0400

    added caching strategy

--- js/script.js
+++ js/script.js
@@ -11,4 +11,47 @@ else{
     console.log("Service worker is not found!!!")
 }
 
+//load data from the web
 
+function getPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(json => renderPosts(json))
+}
+
+
+
+function renderPosts(posts){
+    const output=document.getElementById('output');
+    output.innerHTML =``;
+    for(const post of posts){
+        console.log(post);
+        output.innerHTML +=`
+        <div class="post-container">
+            <h3>${post.title}</h3>
+            <div>${post.body}</div>
+        </div>
+        `
+    }
+}
+
+function renderOffline(){
+    const output=document.getElementById('output');
+        output.innerHTML +=`
+        <div class="post-container">
+           Please check your cnnection.
+        </div>
+        `
+}
+
+
+if(navigator.onLine){
+    getPosts()
+}
+else{
+   renderOffline()
+}
+
+window.addEventListener('online',function(){
+    getPosts();
+})
--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,102 @@
+const cacheName='v2';
+
+
 //called when sw is installed
 self.addEventListener('install',function(event){
-    console.log('SW installed:',event)
+    // console.log('SW installed:',event)
     self.skipWaiting();
-})
+
+    // console.log('cache from sw',caches);
+
+    event.waitUntil(
+    caches.open(cacheName)
+        .then(function(cache){
+            cache.addAll([
+                '/my-pwa/',
+                '/my-pwa/index.html',
+                '/my-pwa/pic2.png',
+                '/my-pwa/css/style.css',
+                // '/my-pwa/manifest.json',
+                '/my-pwa/js/script.js',
+                '/my-pwa/icons/favicon-32x32.png',
+                '/my-pwa/icons2/android-chrome-144x144.png'
+            ]);
+        })
+    );
+});
 
 
 //called when sW activated
 self.addEventListener('activate',function(event){
     console.log('SW Activated:0',event)
     event.waitUntil(clients.claim());
+
+    //delete old caches
+    event.waitUntil(
+        caches.keys()
+        .then(function(cacheNames){
+            console.log(cacheNames)
+            for(const item of cacheNames){
+                if(item!==cacheName){
+                caches.delete(item)
+            }
+            }
+        })
+    )
 })
 
 //every time something comes from the web
 self.addEventListener('fetch',function(event){
-    return;
+
+    //----cache only
+    // event.respondWith(
+    //     caches.match(event.request)
+    //     .then(function(response){
+    //         return response;
+    // })
+    // )
+
+    //----cache with network fallback.
+    //  event.respondWith(
+    //     caches.match(event.request)
+    //     .then(function(response){
+    //         if(response){
+    //             return response;
+    //         }
+    //         else{
+    //             return fetch(event.request);
+    //         }      
+    //     })
+    // )
+
+    //--network with cache fallback
+    // event.respondWith(
+    //     fetch(event.request)
+    //         .then(function(response){
+    //             return response;
+    //         })
+    //         .catch(function(){
+    //              return caches.match(event.request);
+    //         }) 
+    // )
+
+
+    //--stale caching
+    event.respondWith(
+        caches.open(cacheName)
+        .then(function(cache){
+            return cache.match(event.request)
+            .then(function(cachedResponse){
+                const fetchedResponse=fetch(event.request)
+                .then(function(networkResponse){
+                    cache.put(
+                        event.request,
+                        networkResponse
+                    )
+                    return networkResponse;
+                })
+                return cachedResponse || fetchedResponse;
+            })
+        })
+    ) 
 });
