97d8eaaf807c2e35a72d2c940460e46c34496e0a
palak patel <palakp909@gmail.com>
2022-06-03 23:00:59 -0400

    some changes in SW

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('../service-worker.js')
+    navigator.serviceWorker.register('../service-worker.js',{ scope:'/' })
     .then((registration) => {
         console.log('Success. Scope:', registration.scope);
     })
     .catch((error) => {
         console.log('Failed. Error:', error);
     });
+}
+else{
+    console.log("Service worker is not found!!!")
 }
 
 
--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,17 @@
-self.addEventListener('fetch',()=>{
+//called when sw is installed
+self.addEventListener('install',function(event){
+    console.log('SW installed:',event)
+    self.skipWaiting();
+})
+
+
+//called when sW activated
+self.addEventListener('activate',function(event){
+    console.log('SW Activated:0',event)
+    event.waitUntil(clients.claim());
+})
+
+//every time something comes from the web
+self.addEventListener('fetch',function(event){
     return;
 });
